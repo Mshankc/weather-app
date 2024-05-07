a44cef3736239f44051ddab1530fca589cbf1498
mshan <mshankc@gmail.com>
2024-05-07 15:31:51 +0530

    2Nd commit

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
@@ -18,6 +19,17 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await Location().getCurrentLocation();
+    print('getlocation is triggered');
+    print('lat = ${location.latitude}');
+    print('lon = ${location.longitude}');
+  }
+
+  void getData() async {
+    Response response = await get(
+      Uri.parse(
+          'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b1b15e88fa797225412429c1c50c122a1'),
+    );
+    print(response.body);
   }
 
   @override
@@ -25,7 +37,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            getData();
+          },
           child: const Text('Get weather'),
         ),
       ),
--- lib/services/location.dart
+++ lib/services/location.dart
@@ -1,20 +1,19 @@
 import 'package:geolocator/geolocator.dart';
 
 class Location {
-  late double latitude;
-  late double longitude;
+  late double latitude = 0.0;
+  late double longitude = 0.0;
 
   Future<void> getCurrentLocation() async {
     try {
       Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      bool isLocationServiceEnabled =
-          await Geolocator.isLocationServiceEnabled();
-      LocationPermission permission = await Geolocator.requestPermission();
+          desiredAccuracy: LocationAccuracy.low,
+          forceAndroidLocationManager: true);
+
       latitude = position.latitude;
       longitude = position.longitude;
     } catch (e) {
-      print(e);
+      print('ERROR $e');
     }
   }
 }
